18dd61eedab95227f97cc9a41c5833be1ac418ce
Ronald Suez <rsuez93@gmail.com>
2015-10-27 16:21:48 -0430

    proper config of sync delay

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,10 +8,11 @@ s = Rufus::Scheduler.singleton
 # # Enviar correos para confirmar cita diariamente desde 3 
 # # dias antes de las cita hasta el dia anterior o hasta que confirme
 # #
-s.every '1.m' do
-	puts "rufus hello"
-end
-s.every '1m' do
+# s.every '30m' do
+# 	puts "rufus hello"
+# end
+
+s.every '10m' do
   Rails.logger.info "Sending reminders, it's #{Time.now}"
   system 'bundle exec rake slack:post_to_channel'
   #Resque.enqueue(PostToSlack) 
